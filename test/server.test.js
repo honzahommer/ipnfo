2ae1dbd62159729d6f6c82c411d946d4ad4a864e
Honza Hommer <honza@hommer.cz>
2019-02-24 17:34:08 +0100

    Use supertest for tests, response type by accept header

--- test/server.test.js
+++ test/server.test.js
@@ -1,36 +1,29 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const { expect } = require('chai');
+const request = require('supertest');
 const server = require('../lib/server.js');
-// eslint-disable-next-line no-unused-vars
-const should = chai.should();
-
-chai.use(chaiHttp);
 
 describe('ipnfo', () => {
-  describe('/GET 8.8.8.8', done => {
-    it('it should GET 8.8.8.8 GeoIP records', () => {
-      chai.request(server)
-        .get('/8.8.8.8')
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          res.body.should.have.property('hostname').eql('google-public-dns-a.google.com');
-
-          return done();
-        });
-    });
+  it('/GET 8.8.8.8', done => {
+    request(server)
+      .get('/8.8.8.8')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.hostname).to.be.equal('google-public-dns-a.google.com');
+        done();
+      });
   });
 
-  describe('/GET 8.8.8.8/hostname', done => {
-    it('it should GET 8.8.8.8 GeoIP hostname', () => {
-      chai.request(server)
-        .get('/8.8.8.8/hostname')
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.text.should.eql('"google-public-dns-a.google.com"');
-
-          return done();
-        });
-    });
+  it('/GET 8.8.8.8/hostname', done => {
+    request(server)
+      .get('/8.8.8.8/hostname')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.text).to.be.equal('google-public-dns-a.google.com');
+        done();
+      });
   });
 });
